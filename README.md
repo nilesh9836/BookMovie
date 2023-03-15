30f1d5b632d1605a1611d39b627dea5ad406920d
Nilesh Mishra <55555595+nilesh9836@users.noreply.github.com>
2023-03-16 02:22:42 +0530

    search by movie title

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BookMovie
 A web app for search movies by either title or imdbId.
 
+During search we get some suggestion by selecting these suggestion we can get the information about a movie.
 We can mark movie as favourite by click on heart icon.
 By click on show favourite we can see the favourite movies list.
 
--- imdb-app/src/components/SearchBox.vue
+++ imdb-app/src/components/SearchBox.vue
@@ -5,7 +5,25 @@
       <v-select height="57" v-model="searchType" :items="searchTypes" outlined chips hide-details></v-select>
     </v-col>
     <v-col class="pl-0">
-      <v-text-field v-model="searchTxt" @change="onSearch" outlined placeholder="Search by title or imdbId"></v-text-field>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field v-model="searchTxt" @input="handleSearch" v-on="on" outlined placeholder="Search by title or imdbId"></v-text-field>
+
+        </template>
+
+        <v-card background="purple" z-index="999">
+          <ul v-if="searchResults.length > 0">
+            <li v-for="result in searchResults" :key="result.id" @click="selectResult(result)">
+              {{ searchType==="Title"?result.Title : result.imdbID }}
+            </li>
+          </ul>
+          <ul v-if="searchResults.length === 0 && searchTermResults.length > 0">
+            <li v-for="result in searchTermResults" :key="result.id" @click="selectResult(result)">
+              {{ searchType==="Title"?result.Title : result.imdbID }}
+            </li>
+          </ul>
+        </v-card>
+      </v-menu>
     </v-col>
   </v-row>
 </v-container>
@@ -19,15 +37,62 @@ export default {
     return {
       searchTxt: "",
       searchType: "Title",
-      searchTypes: ["Title", "IMDBIds"]
+      searchTypes: ["Title", "IMDBIds"],
+      searchResults: [],
+      searchTermResults: []
     }
   },
   methods: {
-    onSearch() {
+    search(result) {
       this.$emit('search', {
-        searchTxt: this.searchTxt,
+        searchTxt: this.searchType === "Title" ? result.Title : result.imdbID,
         type: this.searchType
       });
+    },
+    getUrl(val) {
+      let url = "http://www.omdbapi.com/?apikey=d3ff6b3d";
+      switch (val.type) {
+        case "Title":
+          url += `&t=${val.searchTxt}`;
+          break;
+        case "IMDBIds":
+          url += `&i=${val.searchTxt}`
+      }
+      return url;
+    },
+    onSearch(val, calledFrom) {
+      this.favMovieList = [];
+      fetch(this.getUrl(val))
+        .then((r) => r.json())
+        .then((resp) => {
+          if (calledFrom === 'term') this.searchTermResults.push(resp);
+          else this.searchResults.push(resp);
+        })
+    },
+    handleSearch() {
+      if (this.searchTxt.length >= 3) {
+        this.onSearch({
+          searchTxt: this.searchTxt,
+          type: this.searchType
+        });
+      } else {
+        this.searchResults = [];
+      }
+    },
+    selectResult(result) {
+      this.search(result);
+    }
+  },
+  watch: {
+    searchTxt() {
+      if (this.searchTxt.length < 3) {
+        this.onSearch({
+          searchTxt: this.searchTxt,
+          type: this.searchType
+        }, 'term');
+      } else {
+        this.searchTermResults = [];
+      }
     }
   }
 }
